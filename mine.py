d8a8b4842c7af9c4858c25cb00ef5a2b4747c248
Vermille <vermillesaw@yahoo.com>
2016-01-06 13:43:35 +0800

    combined crawling and extracting

--- mine.py
+++ mine.py
@@ -1,23 +1,40 @@
 import urllib2
 import re
 from urllib2 import urlopen, URLError
-from urlparse import urlparse
+from urlparse import urljoin, urlparse
 from argparse import ArgumentParser
 from bs4 import BeautifulSoup, SoupStrainer
 from bs4.element import Tag
 from lxml import etree
 
-def get_next (current):
+base_url = ""
+pages = []
+
+def parse_arguments():
+	parser = ArgumentParser(description='Accept Keywords from Users')
+	parser.add_argument('-u', '--url', help='URL of webpage to scrape from', required=True)
+	args = parser.parse_args()
+	global base_url
+	global domain
+	base_url = args.url
+	get_links(base_url)
+
+def get_links(url):
+	if url.startswith(base_url) == False:
+		return
+
 	try:
-		for child in current.children:
-			return child
-	except AttributeError:
-		if current.nextSibling!=None:
-			return current.nextSibling
-		if current.parent!=None:
-			if current.parent.nextSibling!=None:
-				return current.parent.nextSibling
-	return None
+		resp = urlopen(url)
+	except URLError as e:
+		return
+
+	pages.append(url)
+	soup = BeautifulSoup(resp.read(), "lxml")
+	for link in soup.findAll('a'):
+		if link.has_attr('href'):
+			next_link = urljoin(base_url,link['href'])
+			if next_link not in pages:
+				get_links(next_link)
 
 def extract(link):
 	try:
