bb994aa56663f18de266637f74d81ffae4af0b43
Vermille <vermillesaw@yahoo.com>
2016-01-07 12:01:59 +0900

    added NL grammar checking

--- mine.py
+++ mine.py
@@ -2,9 +2,11 @@ from urllib2 import urlopen, URLError
 from urlparse import urljoin, urlparse
 from argparse import ArgumentParser
 from bs4 import BeautifulSoup
+import language_check
 
 base_url = ""
 pages = []
+nl_tool = language_check.LanguageTool('en-US')
 
 def parse_arguments():
 	parser = ArgumentParser(description='Accept Keywords from Users')
@@ -26,17 +28,36 @@ def get_links(url):
 
 	pages.append(url)
 	soup = BeautifulSoup(resp.read(), "lxml")
+	'''
+	#currently checks only the given page
+	#uncomment this part in order to crawl the website
 	for link in soup.findAll('a'):
 		if link.has_attr('href'):
 			next_link = urljoin(base_url,link['href'])
 			if next_link not in pages:
 				get_links(next_link)
-
-	print "page: " + url
+	'''
 	extract(soup)
 
+'''
+finds code samples only based on NL errors
+checking for punctuations to come soon
+'''
 def extract(page):
-	for possible_code in page.findAll('pre'):
-		print possible_code.text
+	for possible_code in page.findAll():
+		if possible_code.name=='p' or possible_code.name=='pre':
+			errors = nl_tool.check(possible_code.text)
+			error_count = 0
+			word_count = len(possible_code.text.split())
+			for error in errors:
+				error_count = error_count + 1
+			error_ratio = float(error_count) / float(word_count)
+			print possible_code.text
+			print "errors: " + str(error_count) + " ratio: " + str(error_ratio)
+			if error_ratio >= 0.5:
+				print "SOURCE CODE \(*O*)/"
+			else:
+				print "NATURAL LANGUAGE (.____.)"
+			print ""
 
 parse_arguments()
